6c2df240dfc0dd70e422688ca869cac7ec3c6641
passedonwisdom <yash.deorah@somaiya.edu>
2021-04-04 02:01:09 +0530

    frint end 1

--- companion_app/views.py
+++ companion_app/views.py
@@ -14,29 +14,6 @@ import geocoder
 # Create your views here.
 
 def index(request):
-    l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
-    'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
-    'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
-    'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
-    'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
-    'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
-    'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
-    'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
-    'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
-    'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
-    'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
-    'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
-    'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
-    'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
-    'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
-    'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
-    'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
-    'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
-    'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
-    'yellow_crust_ooze']
-    # l1.sort()
-    context={'symptoms':l1}
-
     # import pandas as pd
     # df = pd.read_excel("./files/doctor.xlsx")
     # print(df.head())
@@ -50,20 +27,10 @@ def index(request):
     #                             )
 
         # 0 name address latitude logitutde speciality
-
-    return render(request,'index.html',context)
+    return render(request,'index.html')
 
 def predict_disease(request):
-    if(request.method == 'POST'):
-
-        symptom1 = request.POST["symptom1"]
-        symptom2 = request.POST["symptom2"]
-        symptom3 = request.POST["symptom3"]
-        symptom4 = request.POST["symptom4"]
-        symptom5 = request.POST["symptom5"]
-        print(symptom1+symptom2+symptom3+symptom4+symptom5)
-
-        l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
+    l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
         'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
         'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
         'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
@@ -83,7 +50,13 @@ def predict_disease(request):
         'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
         'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
         'yellow_crust_ooze']
-
+    if(request.method == 'POST'):
+        symptom1 = request.POST["symptom1"]
+        symptom2 = request.POST["symptom2"]
+        symptom3 = request.POST["symptom3"]
+        symptom4 = request.POST["symptom4"]
+        symptom5 = request.POST["symptom5"]
+        print(symptom1+symptom2+symptom3+symptom4+symptom5)
         disease=['Fungal infection','Allergy','GERD','Chronic cholestasis','Drug Reaction',
         'Peptic ulcer diseae','AIDS','Diabetes','Gastroenteritis','Bronchial Asthma','Hypertension',
         ' Migraine','Cervical spondylosis',
@@ -157,17 +130,16 @@ def predict_disease(request):
                 if(predicted == a):
                     h='yes'
                     break
-
             if (h=='yes'):
                 print(disease[a])
                 return disease[a]
             else:
                 return "Not Found"
             
-        
         predicted_disease = NaiveBayes()
         context = {'predicted_disease': predicted_disease}
-    return render(request,'output.html',context)
+        return render(request,'output.html',context)
+    return render(request, "symptomsForm.html",{'symptoms':l1})
         
 
 def map_display(request,disease):
