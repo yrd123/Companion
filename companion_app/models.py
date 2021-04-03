51b0f2486448215cb4ae77b23f8c4b1f00131ebd
manish103p <manish.parihar@somaiya.edu>
2021-04-03 22:31:37 +0530

    doc model

--- companion_app/models.py
+++ companion_app/models.py
@@ -11,7 +11,7 @@ class Doctor(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     # userName = models.CharField(max_length=255)
-    longitude = models.CharField(unique=True)
+    longitude = models.FloatField(unique=True)
     latitude = models.FloatField(unique=True)
     # password = models.CharField(max_length=20)
     def __str__(self):
