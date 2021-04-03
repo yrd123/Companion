a5c9adb29e0ec73acd5bbc07679369a7a785793b
manish103p <manish.parihar@somaiya.edu>
2021-04-03 22:30:03 +0530

    doc model

--- companion_app/models.py
+++ companion_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+import os,pytz
+from django.utils import timezone
+from django.conf import settings
+
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    # userName = models.CharField(max_length=255)
+    longitude = models.CharField(unique=True)
+    latitude = models.FloatField(unique=True)
+    # password = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
